e92f172c6e73378319b1d4fc850379a2c151c802
Aditya <adityakrsingh2604@gmail.com>
2024-07-12 09:32:22 +0530

    about add

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>
+            <h1 className='text-lime-50 text-center'>Contact </h1>
+        </div>
+    )
+}
+
+export default page
