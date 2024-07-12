f284ff3ecf3c81f8738676d9c813b4a0697ddaa9
Aditya <adityakrsingh2604@gmail.com>
2024-07-12 09:38:43 +0530

    add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import ContactForm from "@/components/Contact/ContactForm";
+import LottieAnimation from "@/components/Contact/LottieAnimation";
+import siteMetadata from "@/utils/siteMetaData";
 
-function page() {
+
+export const metadata = {
+    title: "Contact Me",
+    description: `Contact me through the form available on this page or email me at ${siteMetadata.email}`,
+};
+
+
+export default function Contact() {
     return (
-        <div>
-            <h1 className='text-lime-50 text-center'>Contact </h1>
-        </div>
-    )
-}
+        <section className="w-full h-auto md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex  flex-col md:flex-row items-center justify-center text-dark dark:text-light">
 
-export default page
+            <div className="inline-block w-full sm:w-4/5 md:w-2/5 h-full md:border-r-2 border-solid border-dark dark:border-light">
+                <LottieAnimation />
+            </div>
+
+            <div className="w-full  md:w-3/5 flex flex-col items-start justify-center px-5 xs:px-10 md:px-16 pb-8">
+                <h2 className="font-bold capitalize  text-2xl xs:text-3xl sm:text-4xl">
+                    Let's Connect!
+                </h2>
+                <ContactForm />
+            </div>
+
+        </section>
+    );
+}
